4234bd5a12ea11a1e2cc4d2218fde1e3f10190c5
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-07-04 00:38:16 +0100

    localhost remived

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000;
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
-    app.listen(PORT, "localhost", () => {
+    app.listen(PORT, () => {
         console.log("Server is running on port " + PORT);  
     });
 })
